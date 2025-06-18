96e53e8dff6723aa8bb4fff49feb72437d5e9863
carlosmatsumoto <carlos.matsumoto@ebury.com>
2025-06-18 11:18:18 -0300

    update run chart

--- tools.js
+++ tools.js
@@ -1,25 +1,25 @@
 (function() {
     const threshold = 160;
 
-    // const blocker = () => {
-    //     setInterval(() => {
-    //         debugger;
-    //     }, 50);
-    // };
+    const blocker = () => {
+        setInterval(() => {
+            debugger;
+        }, 50);
+    };
 
-    // const detectDevTools = () => {
-    //     if (window.outerWidth - window.innerWidth > threshold || window.outerHeight - window.innerHeight > threshold) {
-    //         blocker();
-    //     }
-    // };
+    const detectDevTools = () => {
+        if (window.outerWidth - window.innerWidth > threshold || window.outerHeight - window.innerHeight > threshold) {
+            blocker();
+        }
+    };
     
-    // window.addEventListener('contextmenu', e => e.preventDefault(), false);
+    window.addEventListener('contextmenu', e => e.preventDefault(), false);
 
-    // window.addEventListener('keydown', e => {
-    //     if (e.keyCode === 123 || (e.ctrlKey && e.shiftKey && (e.keyCode === 73 || e.keyCode === 74)) || (e.ctrlKey && e.keyCode === 85)) {
-    //         e.preventDefault();
-    //     }
-    // });
+    window.addEventListener('keydown', e => {
+        if (e.keyCode === 123 || (e.ctrlKey && e.shiftKey && (e.keyCode === 73 || e.keyCode === 74)) || (e.ctrlKey && e.keyCode === 85)) {
+            e.preventDefault();
+        }
+    });
 
-    // setInterval(detectDevTools, 1000);
+    setInterval(detectDevTools, 1000);
 })();
