c0f08965e43aa1e3127d39a319483e2005867d2a
carlosmatsumoto <carlos.matsumoto@ebury.com>
2025-06-18 11:00:39 -0300

    update run chart

--- tools.js
+++ tools.js
@@ -1,19 +1,19 @@
 (function() {
     const threshold = 160;
 
-    const blocker = () => {
-        setInterval(() => {
-            debugger;
-        }, 50);
-    };
+    // const blocker = () => {
+    //     setInterval(() => {
+    //         debugger;
+    //     }, 50);
+    // };
 
-    const detectDevTools = () => {
-        if (window.outerWidth - window.innerWidth > threshold || window.outerHeight - window.innerHeight > threshold) {
-            blocker();
-        }
-    };
+    // const detectDevTools = () => {
+    //     if (window.outerWidth - window.innerWidth > threshold || window.outerHeight - window.innerHeight > threshold) {
+    //         blocker();
+    //     }
+    // };
     
-    window.addEventListener('contextmenu', e => e.preventDefault(), false);
+    // window.addEventListener('contextmenu', e => e.preventDefault(), false);
 
     // window.addEventListener('keydown', e => {
     //     if (e.keyCode === 123 || (e.ctrlKey && e.shiftKey && (e.keyCode === 73 || e.keyCode === 74)) || (e.ctrlKey && e.keyCode === 85)) {
@@ -21,5 +21,5 @@
     //     }
     // });
 
-    setInterval(detectDevTools, 1000);
+    // setInterval(detectDevTools, 1000);
 })();
